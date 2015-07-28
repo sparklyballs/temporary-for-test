51ed4a53daf7a09d22fd85c8b2d76e14f1b64a61
sparklyballs <sparklyballs@gmail.com>
2015-07-28 11:53:35 +0100

    further work on photoshow, still non-functioning

---
+++ photoshow/init/30_update_get_the_site.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ ! -f "/var/www/PhotoShow/config.php" ]; then
+echo "fetching photoshow files from github"
+git clone https://github.com/thibaud-rohmer/PhotoShow.git /var/www/PhotoShow
+sed -i -e 's/$config->photos_dir.\+/$config->photos_dir = "\/Pictures";/' /var/www/PhotoShow/config.php
+sed -i -e 's/$config->ps_generated.\+/$config->ps_generated = "\/Thumbs";/' /var/www/PhotoShow/config.php
+else
+echo "checking for updates"
+cd /var/www/PhotoShow
+git pull
+fi
+
